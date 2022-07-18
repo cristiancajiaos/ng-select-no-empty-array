200b55fd289884ffb4b8bede35bba75eba8b5bf0
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-07-18 15:39:03 -0400

    Creado formulario básico

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,6 @@
+import { createMayBeForwardRefExpression } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  fooForm: FormGroup;
+
+  names = ['Alpha', 'Beta', 'Gamma', 'Omega'];
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.fooForm = this.fb.group({
+      names: new FormControl([], [Validators.required])
+    });
+  }
+
+  public campoValido(field: string): boolean {
+    let campo = this.fooForm.controls[field];
+    return campo.touched && campo.errors != null && campo.errors['required'];
+  }
+
+  public onSubmit(): void {
+    console.log(this.fooForm.value);
   }
 
 }
